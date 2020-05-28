c9c74b8a6513329ab844a60b5cb2d2af1b5209e4
Arsalan Abrar <abrararsalan@gmail.com>
2020-05-28 23:38:00 +0530

    Initial commit

---
+++ app/src/main/java/com/pawan/triviaapp/SecondQuestionActivity.java
@@ -0,0 +1,14 @@
+package com.pawan.triviaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_question);
+    }
+}
